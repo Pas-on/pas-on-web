476295ca5b4e86e8c145ddecf8886ec4f4953d27
kelvin handoko <kelvinhandoko03@gmail.com>
2023-06-25 03:44:04 +0700

    feat: fix product slider

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const crButtons = document.querySelectorAll("[data-cr-button]")
 const itemContainer = document.querySelector("#item-container")
 const slButtons = document.querySelectorAll("[data-sl-button]")
 const slItem = document.querySelector(".slider-item")
-
+const productSlider = document.getElementById("product-slider")
+const slLeftButton = document.getElementById("sl-left-button")
+const slRightButton = document.getElementById("sl-right-button")
+const productSliderItem = document.querySelectorAll(".slider-item")
 //carousel logic
 
 crButtons.forEach(button => {
@@ -20,11 +23,46 @@ crButtons.forEach(button => {
 })
 
 //product-slider
-slButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        const itemLength = slItem.offsetWidth
-        console.log(itemLength)
-        const offset = button.dataset.slButton === "next" ? `-${itemLength * 5}px` : `0`
-        itemContainer.style.transform = `translateX(${offset})`
-    })
-})
+
+let slidePos = 0
+
+const currentItemsLength = getComputedStyle(productSlider).getPropertyValue("--max-item")
+
+checkBoundary()
+
+function translateSlider() {
+    checkBoundary()
+
+    itemContainer.style.transform = `translateX(${slidePos}px)`
+}
+
+function slideToNext() {
+    slLeftButton.classList.remove("hidden")
+    slidePos -= productSliderItem[0].offsetWidth * currentItemsLength
+    translateSlider()
+}
+
+function slideToPrev() {
+    slidePos += productSliderItem[0].offsetWidth * currentItemsLength
+    translateSlider()
+}
+
+function checkBoundary() {
+    if (slidePos === 0) {
+        slLeftButton.classList.add("hidden")
+    } else {
+        slLeftButton.classList.remove("hidden")
+    }
+
+    if (
+        Math.abs(slidePos) + productSliderItem[0].offsetWidth * currentItemsLength >=
+        productSliderItem[0].offsetWidth * productSliderItem.length
+    ) {
+        slRightButton.classList.add("hidden")
+    } else {
+        slRightButton.classList.remove("hidden")
+    }
+}
+
+slLeftButton.addEventListener("click", slideToPrev)
+slRightButton.addEventListener("click", slideToNext)

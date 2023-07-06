685c15772c8e31e0d192ec0be4221e519378e840
kelvin handoko <kelvinhandoko03@gmail.com>
2023-07-07 02:11:32 +0700

    feat: add user.json and login logic

--- login/login.js
+++ login/login.js
@@ -12,3 +12,41 @@ eyeIcon.addEventListener("click", () => {
         passwordInput.type = "password"
     }
 })
+
+// form logic
+import users from "../data/user.json" assert { type: "json" }
+const form = document.querySelector("form")
+const errorBox = document.querySelector(".error-box")
+const errorText = "user atau password salah"
+const submitButton = document.querySelector("button")
+
+const fakeLoading = () => {
+    const spinner = document.createElement("i")
+    spinner.classList.add("fa", "fa-spinner", "fa-spin")
+    submitButton.innerText = ""
+    submitButton.appendChild(spinner)
+}
+
+const addErrorText = () => {
+    errorBox.classList.add("show")
+    errorBox.innerText = errorText
+}
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    const { name, password } = e.target.elements
+    const findUser = users.find(user => user.name === name.value)
+    if (!findUser) {
+        addErrorText()
+    } else {
+        const checkPass = findUser?.password === password.value
+        if (!checkPass) {
+            addErrorText()
+        } else {
+            fakeLoading()
+            setInterval(() => {
+                location.href = "/"
+            }, 500)
+        }
+    }
+})

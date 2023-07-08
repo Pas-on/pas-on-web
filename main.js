36090d664439a09c6832135cf23ec36f9c7c31d4
kelvin handoko <kelvinhandoko03@gmail.com>
2023-07-09 05:09:49 +0700

    feat: add inject html"

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { ProductSlider } from "./slider.js"
-
+import products from "./data/products.json" assert { type: "json" }
 const crButtons = document.querySelectorAll("[data-cr-button]")
 
 const button = document.getElementById("hamburger-button")
@@ -32,6 +32,33 @@ const meatLeftButton = document.getElementById("meat-left-button")
 const meatRightButton = document.getElementById("meat-right-button")
 new ProductSlider(meatItemsWrapper, meatLeftButton, meatRightButton)
 
+// sayuran-product-slider
+const sayuranItemsWrapper = document.getElementById("sayuran-items")
+const sayuranItemsContainer = sayuranItemsWrapper.querySelector(".item-container")
+products.forEach(product => {
+    const child = document.createElement("div")
+    child.classList.add("slider-item", "elHidden")
+    child.innerHTML = `<div class="image-wrapper new">
+    <img src="assets/${product.image}" alt="sayuran" loading="lazy" />
+    <div class="product-action ">
+        <i class="fa-solid fa-heart wishlist fa-xl" data-id=${product.id}></i>
+    </div>
+</div>
+<div class="product-desc">
+    <div>
+        <p class="product-price">${new Intl.NumberFormat("id-ID", {
+            style: "currency",
+            currency: "IDR",
+        }).format(product.price)}</p>
+        <p class="product-weight">~${product.size}</p>
+    </div>
+    <span class="product-title"> ${product.title} </span>
+</div>
+<button class="product-cart-button" data-id=${product.id} >add to cart</button>
+`
+    sayuranItemsContainer.appendChild(child)
+})
+
 //drawer logic
 button.addEventListener("change", () => {
     if (button.checked) {
@@ -48,7 +75,6 @@ button.addEventListener("change", () => {
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-            console.log("intesecting")
             entry.target.style.animation = "var(--fade-in-animation)"
         }
     })
@@ -56,3 +82,39 @@ const observer = new IntersectionObserver(entries => {
 
 const hiddenElements = document.querySelectorAll(".elHidden")
 hiddenElements.forEach(el => observer.observe(el))
+
+// add-to-wishlist logic
+const wishlistButtons = document.querySelectorAll(".wishlist")
+// get wishlist item dr localStorage
+wishlistButtons.forEach(button => {
+    button.addEventListener("click", e => {
+        const wishlistItems = localStorage.getItem("wishlists") ? JSON.parse(localStorage.getItem("wishlists")) : []
+        const id = e.target.dataset.id
+        const checkId = wishlistItems.includes(id)
+        if (!checkId) {
+            wishlistItems.push(id)
+            localStorage.setItem("wishlists", JSON.stringify(wishlistItems))
+            button.classList.add("active")
+        } else {
+            const updatedWishlistItems = wishlistItems.filter(item => item !== id)
+            localStorage.setItem("wishlists", JSON.stringify(updatedWishlistItems))
+            button.classList.remove("active")
+        }
+    })
+})
+
+// add-to-cart logic
+const cartButtons = document.querySelectorAll(".product-cart-button")
+
+// get cart item dr localStorage
+const cartItems = localStorage.getItem("carts") ? JSON.parse(localStorage.getItem("carts")) : []
+cartButtons.forEach(button => {
+    button.addEventListener("click", e => {
+        const id = e.target.dataset.id
+        const checkId = cartItems.includes(id)
+        if (!checkId) {
+            cartItems.push(id)
+            localStorage.setItem("carts", JSON.stringify(cartItems))
+        }
+    })
+})

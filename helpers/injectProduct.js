7f1fcebbd1ff3133e14a44d12ecaa0bcd5d2123d
kelvin handoko <kelvinhandoko03@gmail.com>
2023-07-10 02:25:11 +0700

    update

--- helpers/injectProduct.js
+++ helpers/injectProduct.js
@@ -1,4 +1,30 @@
-import products from "../data/products.json" assert { type: "json" }
+// import products from "../data/products.json" assert { type: "json" }
+const products = [
+    {
+        "id": 1,
+        "title": "aneka sayuran",
+        "price": 150000,
+        "size": "700gr",
+        "category": "sayuran",
+        "image": "/aneka_sayuran-removebg-preview.png",
+    },
+    {
+        "id": 2,
+        "title": "bawang putih jepang",
+        "price": 70000,
+        "size": "350gr",
+        "category": "new",
+        "image": "/bawang_putih-removebg-preview.png",
+    },
+    {
+        "id": 3,
+        "title": "beras import jepang kualitas terbaik",
+        "price": 125000,
+        "size": "10kg",
+        "category": "sayuran",
+        "image": "/beras-removebg-preview.png",
+    },
+]
 
 const injectProduct = (category, productContainer) => {
     const filteredProduct = products.filter(product => product.category === category)
